2bc0bf0802b61b50bbf744411f8523240e92063b
윤상묵 <smyuau@gmail.com>
2024-11-19 09:48:29 +0900

    SignInView, SignIn 기능 추가, AppNavBar 작성, HomeView 홈페이지 작성

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import AppNavBar from '@/components/Common/AppNavBar.vue'
+import HomeView from '@/views/HomeView.vue'
+
 </script>
 
 <template>
+  <AppNavBar />
+  <div>
+    <h1>MoFin MoFin MoFin</h1>
+  </div>
   <RouterView />
 </template>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,28 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 import SignUpView from '@/views/SignUpView.vue'
+import SignInView from '@/views/SignInView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
       path: '/signup',
       name: 'signUp',
       component: SignUpView
     },
-
+    {
+      path: '/signin',
+      name: 'signIn',
+      component: SignInView
+    }
   ]
 })
 
-// router.beforeEach((to, from) => {
-//   const store = useAuthStore()
-//   if (to.name === 'home' && !store.isLogin) {
-//     window.alert('로그인이 필요합니다.')
-//     return { name: 'signIn'}
-//   }
-//   if ((to.name === 'signUp' || to.name === 'signIn') && (store.isLogin)) {
-//     window.alert('이미 로그인이 되어 있습니다.')
-//     return { name: 'home' }
-//   }
-// })
+router.beforeEach((to, from) => {
+  const store = useAuthStore()
+  if ((to.name === 'signUp' || to.name === 'signIn') && (store.isLogin)) {
+    window.alert('이미 로그인이 되어 있습니다.')
+    return { name: 'home' }
+  }
+})
 export default router
