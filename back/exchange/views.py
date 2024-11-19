d5cbb66d757266dad42213789802db9c72b5e46d
김홍범 <fkdldj48@naver.com>
2024-11-19 23:23:56 +0900

    환율 계산, 은행 위치, ReadMe

--- back/exchange/views.py
+++ back/exchange/views.py
@@ -1,34 +1,32 @@
 import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# from rest_framework.permissions import IsAuthenticated
 from django.conf import settings
 
 class ExchangeRateAPIView(APIView):
-    # permission_classes = [IsAuthenticated]
-
     def get(self, request, format=None):
-        # API 키 및 기본 URL
-        api_key = settings.EXIMBANK_API_KEY  # settings.py에 저장된 API 키
+        api_key = settings.EXIMBANK_API_KEY
         url = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON"
-
-        # 요청 파라미터
         params = {
             "authkey": api_key,
-            "searchdate": request.query_params.get('date', '20241119'),  # YYYYMMDD 형식
-            "data": "AP01"  # 환율 데이터 타입
+            "searchdate": request.query_params.get('date', '20241119'),
+            "data": "AP01"
         }
 
-        response = requests.get(url, params=params)
-        if response.status_code == 200:
-            data = response.json()
-            target_currency = request.query_params.get('target', 'USD')  # 목표 통화
-            for item in data:
-                if item['cur_unit'] == target_currency:
-                    return Response({
-                        'currency': target_currency,
-                        'base': "KRW",
-                        'rate': item['deal_bas_r']  # 매매기준율
-                    })
-            return Response({'error': 'Invalid target currency'}, status=400)
-        return Response({'error': 'API request failed'}, status=500)
+        try:
+            response = requests.get(url, params=params, verify=True)
+            if response.status_code == 200:
+                data = response.json()
+                target_currency = request.query_params.get('target', 'USD')
+
+                for item in data:
+                    if item['cur_unit'] == target_currency:
+                        return Response({
+                            'currency': target_currency,
+                            'base': "KRW",
+                            'rate': item['deal_bas_r']
+                        })
+                return Response({'error': 'Invalid target currency'}, status=400)
+            return Response({'error': 'API request failed'}, status=500)
+        except Exception as e:
+            return Response({'error': str(e)}, status=500)
