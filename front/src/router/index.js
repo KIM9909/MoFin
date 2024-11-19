72f3961c23088a0a5830e66971367a198b8a927a
윤상묵 <smyuau@gmail.com>
2024-11-19 16:32:59 +0900

    예금 종류 페이지 구성, 상세 보기 클릭 시 이자율, 금리 등 상세 정보가 토글될 수 있도록 구성

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -5,7 +5,7 @@ import SignInView from '@/views/SignInView.vue'
 import HomeView from '@/views/HomeView.vue'
 import LocationView from '@/views/LocationView.vue'
 import InterestRateView from '@/views/InterestRateView.vue'
-import ExchangeRateView from '@/views/exchangeRateView.vue'
+import ExchangeRateView from '@/views/ExchangeRateView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
