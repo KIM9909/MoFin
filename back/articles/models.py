8d1e645415691b46bd554e012d10387802f35dce
김홍범 <fkdldj48@naver.com>
2024-11-20 20:41:24 +0900

    게시물 댓글 기능 구현

--- back/articles/models.py
+++ back/articles/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
 class Articles(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -9,4 +9,13 @@ class Articles(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+class Comment(models.Model):
+    article = models.ForeignKey(Articles, related_name="comments", on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Comment on {self.article.title}: {self.content[:20]}"
--- back/articles/serializers.py
+++ back/articles/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Articles
+from .models import Articles, Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'content', 'created_at', 'article', 'user']
+        read_only_fields = ['user']
 
 class ArticleSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)  # 관련 댓글 표시
+
     class Meta:
         model = Articles
-        fields = ['id', 'title', 'content']
+        fields = ['id', 'title', 'content', 'comments', 'created_at', 'updated_at']
